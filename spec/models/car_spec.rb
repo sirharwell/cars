aeb36c3bac14e999d1119e78268996240df526aa
Ian Harwell <sirharwell@gmail.com>
2018-04-05 10:54:00 -0600

    rspec

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Car, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'attributes' do
+    it { should respond_to :make }
+    it { should respond_to :model }
+    it { should respond_to :price }
+    it { should respond_to :color }
+    it { should respond_to :interior }
+    it { should respond_to :mileage }
+    it { should respond_to :runs }
+    end
 end
